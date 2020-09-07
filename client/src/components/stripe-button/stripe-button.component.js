31a1710d7f2f04db16aad333d69eb3eff57ea635
Henry <ibooshyou@gmail.com>
2020-09-07 11:25:35 +0100

    added express server to handle stripe payment, seperated client from backend

--- client/src/components/stripe-button/stripe-button.component.js
+++ client/src/components/stripe-button/stripe-button.component.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_51HAEzlAQAr0oNZHrw3oYK4t38GGD3kyv2At1dZXkDivkmoM6YpwLKNb2RaoyqtpaLFfKYmabXswhu7BIpDz09JRK00QJPvedPP';
     const onToken = (token) => {
-        console.log(token)
-        alert('Payment Successful')
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        }).then(response => {
+            alert('Payment successful')
+        }).catch(error => {
+            console.log('Payment error: ', JSON.parse(error))
+            alert('There was an issue with your payment. Please make sure you use the provided test credit card')
+        })
     }
 
     return (
@@ -21,6 +33,7 @@ const StripeCheckoutButton = ({ price }) => {
            panelLabel='Pay Now'
            token={onToken}
            stripeKey={publishableKey}
+           currency="GBP"
         />
     )
 }
