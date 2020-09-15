7600a9ca2afff07b273f13eda2bcb038237a86f0
Henry <ibooshyou@gmail.com>
2020-09-15 19:43:07 +0100

    added sagas, actions, types to cart redux for firebase specific user carts

--- client/src/redux/cart/cart.reducer.js
+++ client/src/redux/cart/cart.reducer.js
@@ -1,4 +1,4 @@
-import { CartActionTypes } from './cart.types';
+import CartActionTypes from './cart.types';
 import { addItemToCart, removeItemFromCart } from './cart.utils' 
 
 const INITIAL_STATE = {
@@ -33,6 +33,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
                 ...state,
                 cartItems: []
             }
+        case CartActionTypes.SET_CART_FROM_FIREBASE:
+            return {
+                ...state,
+                cartItems: action.payload
+            };
         default:
             return state;
     }
