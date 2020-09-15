f2fa6cecd1263684ee0112de1bc2e723f3558db5
Henry <ibooshyou@gmail.com>
2020-09-15 18:58:34 +0100

    testing for heroku performance

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-//const compression = require('compression');
+const compression = require('compression');
 const enforce = require('express-sslify');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
@@ -11,14 +11,17 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
 
+
+
 if(process.env.NODE_ENV === 'production') {
-    //app.use(compression());
-    app.use(enforce.HTTPS({ trustProtoHeader: true }));
+    app.use(compression());
+    
     app.use(express.static(path.join(__dirname, 'client/build')));
 
     app.get('*', function(req, res) {
